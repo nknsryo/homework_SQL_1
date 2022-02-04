80252cc4ae0b32a0b276bba2c56245a5bdf9c5b1
nknsryo <nkns.ryo@gmail.com>
2022-02-04 13:05:37 +0900

    こんな感じ

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+VALUES
+    ('Bob',15),
+    ('Tom',57),
+    ('Ken',73)
--- schema.sql
+++ schema.sql
@@ -1,4 +1,12 @@
-CREATE TABLE users(
+DROP TABLE IF EXISTS users;
+--初期化
+CREATE TABLE IF NOT EXISTS users(
             name TEXT,
             age INTEGER
             );
+--テーブル作る
+INSERT INTO users
+VALUES
+    ('Bob',15),
+    ('Tom',57),
+    ('Ken',73)
